0a1022a946eb279942a12497bdf5c6569c9cd359
pratik-brp <pratik@ubventuresllc.com>
2026-07-13 11:58:07 +0545

    logo splash for 3 seconds added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from 'react'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import ErrorBoundary from '../components/ErrorBoundary'
 import SkipToContent from '../components/SkipToContent'
 
 export const Route = createRootRoute({
-  component: () => (
-    <ErrorBoundary>
-      <SkipToContent />
-      <Outlet />
-    </ErrorBoundary>
-  ),
+  component: () => {
+    const [ready, setReady] = useState(false)
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        const loader = document.getElementById('app-loader')
+        if (loader) loader.style.opacity = '0'
+        setReady(true)
+        setTimeout(() => document.getElementById('app-loader')?.remove(), 600)
+      }, 3000)
+      return () => clearTimeout(timer)
+    }, [])
+
+    return (
+      <ErrorBoundary>
+        <SkipToContent />
+        {ready && <Outlet />}
+      </ErrorBoundary>
+    )
+  },
 })
